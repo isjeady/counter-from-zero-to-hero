0b3c7b1870929d2f355c5910db7d3d8fe4126ea5
Isjeady <admin@isjeady.com>
2023-12-01 15:32:05 +0100

    Prisma Map 2

--- button-counter-ts/prisma/migrations/20231201143127_init/migration.sql
+++ button-counter-ts/prisma/migrations/20231201143127_init/migration.sql
@@ -1,13 +1,13 @@
 -- CreateTable
-CREATE TABLE "Setting" (
+CREATE TABLE "settings" (
     "id" SERIAL NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "key" TEXT NOT NULL,
     "value" TEXT NOT NULL,
 
-    CONSTRAINT "Setting_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "settings_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Setting_key_key" ON "Setting"("key");
+CREATE UNIQUE INDEX "settings_key_key" ON "settings"("key");
